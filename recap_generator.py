30685de7a70ef44e8352c1fd11f09d141b83fcf8
Daniel Berlyne <danberlyne@gmail.com>
2023-08-13 10:29:55 +0100

    fixed xlsx data not being identified as strings

--- recap_generator.py
+++ recap_generator.py
@@ -11,10 +11,10 @@ from moviepy.video.tools.subtitles import SubtitlesClip
 wb = openpyxl.load_workbook('video_data.xlsx')
 ws = wb.active
 id_cells = [cell for cell in ws['A'] if cell.value != 'FILENAME']
-ids = [cell.value for cell in ws['A'] if cell.value != 'FILENAME']
+ids = [str(cell.value) for cell in ws['A'] if cell.value != 'FILENAME']
 
 # Pick out the specified clips from the files and normalise their audio.
-video_clips = [VideoFileClip(str(Path('Videos', f'{id}'))).subclip(ws[f'B{id_cells[ids.index(id)].row}'].value, ws[f'C{id_cells[ids.index(id)].row}'].value).fx(afx.audio_normalize) for id in ids]
+video_clips = [VideoFileClip(str(Path('Videos', f'{id}'))).subclip(str(ws[f'B{id_cells[ids.index(id)].row}'].value), str(ws[f'C{id_cells[ids.index(id)].row}'].value)).fx(afx.audio_normalize) for id in ids]
 
 # Resize clips while maintaining aspect ratio and then add black borders if necessary to reach 1920x1080p.
 resized_clips = [CompositeVideoClip([ImageClip('1920x1080-black.jpg', duration=clip.duration).resize((1920, 1080)), clip.resize(height=1080).set_position('center', 'center')]) if clip.w / clip.h <= 1920 / 1080 else CompositeVideoClip([ImageClip('1920x1080-black.jpg', duration=clip.duration), clip.resize(width=1920).set_position('center', 'center')]) for clip in video_clips]
@@ -32,7 +32,7 @@ video = CompositeVideoClip(faded_clips)
 generator = lambda txt: TextClip(txt, font='Arial', fontsize=50, color='white')
 subs = [((int(sum(previous_clip.duration - custom_padding for previous_clip in video_clips[:video_clips.index(clip)])) + custom_padding, # Start time of subtitle
         int(sum(previous_clip.duration - custom_padding for previous_clip in video_clips[:video_clips.index(clip)+1]))), # End time of subtitle
-        ws[f'D{id_cells[video_clips.index(clip)].row}'].value + '\n' + ws[f'E{id_cells[video_clips.index(clip)].row}'].value + '\n' + ws[f'F{id_cells[video_clips.index(clip)].row}'].value) # Text of subtitle
+        str(ws[f'D{id_cells[video_clips.index(clip)].row}'].value) + '\n' + str(ws[f'E{id_cells[video_clips.index(clip)].row}'].value) + '\n' + str(ws[f'F{id_cells[video_clips.index(clip)].row}'].value)) # Text of subtitle
         for clip in video_clips]
 subtitles = SubtitlesClip(subs, generator)
 recap = CompositeVideoClip([video, subtitles.set_pos(('center','bottom'))])
