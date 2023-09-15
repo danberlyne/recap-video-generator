6c6bf697363fa8d538d1f2360096012ea5a73cda
Daniel Berlyne <danberlyne@gmail.com>
2023-09-15 11:39:12 +0100

    added option to align subtitles left/centre/right

--- recap_generator.py
+++ recap_generator.py
@@ -5,9 +5,10 @@ import openpyxl
 from pathlib import Path
 from moviepy.editor import *
 import moviepy.audio.fx.all as afx
+import moviepy.video.fx.all as vfx
 from moviepy.video.tools.subtitles import SubtitlesClip
 
-def main():
+def main(alignment):
     # Get filenames from spreadsheet.
     wb = openpyxl.load_workbook('video_data.xlsx')
     ws = wb.active
@@ -27,19 +28,45 @@ def main():
     for clip in resized_clips[1:]:
         faded_clips.append(clip.set_start(idx).crossfadein(custom_padding))
         idx += clip.duration - custom_padding
+    # Fade the final clip out to black.
+    faded_clips[-1] = vfx.fadeout(faded_clips[-1], custom_padding)
+    faded_clips[-1] = afx.audio_fadeout(faded_clips[-1], custom_padding)
     video = CompositeVideoClip(faded_clips)
 
     # Add subtitles.
+    subtitles = []
     generator = lambda txt: TextClip(txt, font='Arial', fontsize=50, color='white')
     subs = [((int(sum(previous_clip.duration - custom_padding for previous_clip in video_clips[:video_clips.index(clip)])) + custom_padding, # Start time of subtitle
             int(sum(previous_clip.duration - custom_padding for previous_clip in video_clips[:video_clips.index(clip)+1]))), # End time of subtitle
-            str(ws[f'D{id_cells[video_clips.index(clip)].row}'].value) + '\n' + str(ws[f'E{id_cells[video_clips.index(clip)].row}'].value) + '\n' + str(ws[f'F{id_cells[video_clips.index(clip)].row}'].value)) # Text of subtitle
+            str(ws[f'D{id_cells[video_clips.index(clip)].row}'].value) + '\n' + ' ' + '\n' + ' ') # Text of subtitle
             for clip in video_clips]
-    subtitles = SubtitlesClip(subs, generator)
-    recap = CompositeVideoClip([video, subtitles.set_pos(('center','bottom'))])
+    subtitles.append(SubtitlesClip(subs, generator))
+
+    generator = lambda txt: TextClip(txt, font='Arial', fontsize=50, color='white')
+    subs = [((int(sum(previous_clip.duration - custom_padding for previous_clip in video_clips[:video_clips.index(clip)])) + custom_padding, # Start time of subtitle
+            int(sum(previous_clip.duration - custom_padding for previous_clip in video_clips[:video_clips.index(clip)+1]))), # End time of subtitle
+            ' ' + '\n' + str(ws[f'E{id_cells[video_clips.index(clip)].row}'].value) + '\n' + ' ') # Text of subtitle
+            for clip in video_clips]
+    subtitles.append(SubtitlesClip(subs, generator))
+
+    generator = lambda txt: TextClip(txt, font='Arial', fontsize=50, color='white')
+    subs = [((int(sum(previous_clip.duration - custom_padding for previous_clip in video_clips[:video_clips.index(clip)])) + custom_padding, # Start time of subtitle
+            int(sum(previous_clip.duration - custom_padding for previous_clip in video_clips[:video_clips.index(clip)+1]))), # End time of subtitle
+            ' ' + '\n' + ' ' + '\n' + str(ws[f'F{id_cells[video_clips.index(clip)].row}'].value)) # Text of subtitle
+            for clip in video_clips]
+    subtitles.append(SubtitlesClip(subs, generator))
+
+    # Align subtitles according to user input.
+    recap = CompositeVideoClip([video] + [sub.set_pos((f'{alignment}','bottom')) for sub in subtitles])
 
     # Save recap video file.
     recap.write_videofile('recap.mp4')
 
 if __name__ == '__main__':
-    main()
+    alignment = ''
+    while alignment not in ('left', 'center', 'right'):
+        print('Please enter your desired subtitle alignment (left/center/right)')
+        alignment = input().lower()
+        if alignment == 'centre':
+            alignment = 'center'
+    main(alignment)
